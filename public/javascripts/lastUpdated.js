51e8241426bd91cd2bb67095a3777b4ce97c0097
sm <santimirandarp@gmail.com>
2021-07-02 09:53:57 +0200

    setting up the basics: index page

---
+++ public/javascripts/lastUpdated.js
@@ -0,0 +1,6 @@
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+lastUpdated=document.getElementById("lastUpdated")
+
+
