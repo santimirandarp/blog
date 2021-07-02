573861ef51324744bceac42096c25e8b6b9f331a
sm <santimirandarp@gmail.com>
2021-07-02 15:27:06 +0200

    doing the style

--- public/javascripts/lastUpdated.js
+++
@@ -1,8 +0,0 @@
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-
-const lastUpdated=document.getElementById("lastUpdated")
-
-var date = new Date() // Date() constructor, instaces date object inherits many methods and props
-lastUpdated.innerHTML = `${months[date.getMonth()]} ${days[date.getDay()]}, ${date.getYear()}`
-
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const date=require('../local_modules/lastUpdated')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Two Travellers', date, firstSec:"Why do we travel", secondSec:"How can we help",thirdSec:"I don't know yet"} );
 });
 
 module.exports = router;
