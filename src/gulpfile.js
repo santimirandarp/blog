d5409226db03abdabadb197ee0116706f05320d7
sm <santimirandarp@gmail.com>
2021-07-05 15:57:33 +0200

    about almost finished

---
+++ src/gulpfile.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const {src,dest,watch,series,parallel}  = require("gulp")
+const imagemin = require("gulp-imagemin")
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const nodemon = require('gulp-nodemon');
+
+
+function cleanFilename(filename, extension){
+  if (typeof(filename)=='string' && typeof(extension)=='string'){
+    //spaces with underscore and ' or " with nothing
+    filename = filename.replace('/\s+/g','_').replace('/[\'\"]/g','')
+      //lowercase the extension
+      extension = extension.toLowerCase()
+      return filename, extension
+  } else{ 
+    throw "One of the parameters is not a string"
+  }
+}
+
+//path
+const tpath = {
+src:{ js:'public/src/javascripts/**/*',
+      images:'public/src/images/**/*.{png,PNG,svg,jpg,jpeg,JPG,JPEG}',
+      scss:'public/src/stylesheets/**/*.scss',
+    },
+dest:{
+js:'public/dist/javascripts/',
+   images:'public/dist/images/',
+   scss:'public/dist/stylesheets/',
+     }}
+
+function genCSS() {
+  //compiles scss and builds a source map useful for debugging in browser
+  //this finds a line in css, and maps it to the source (file and line).
+  return src(tpath.src.scss)
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    // dest will be a single index.css file, and some from folders (blog, about, etc).
+    .pipe(dest(tpath.dest.scss))
+};
+
+function minify(){
+  // minify jpg, png, svg images; 
+  //replace whitespace with underscore
+  //improve the extension (all lowercase)
+  // move them to dist folder
+
+  return src(tpath.src.images)
+    .pipe( imagemin([
+          //imagemin.gifsicle({interlaced: true}),
+          imagemin.mozjpeg({quality: 75, progressive: true}),
+          imagemin.optipng({optimizationLevel: 5}),
+          imagemin.svgo({ plugins: [ {removeViewBox: true}, {cleanupIDs: false} ] })
+    ]))
+    .pipe(rename(function (path) {
+          path.basename, path.extname = cleanFilename(path.basename, path.extname)
+          }))
+  .pipe(dest(tpath.dest.images))
+}
+
+function js(){ 
+  //just copy the js files from src over to dest
+  return src(tpath.src.js).pipe(dest(tpath.dest.js)) 
+}
+
+function watcher (cb) {
+   const stream = nodemon({
+script: './bin/www' , 
+ext: 'js scss ejs',
+ignore: [ 'public/dist/', 'node_modules/' ],
+watch:    [tpath.src.js, tpath.src.scss],
+tasks: function (changedFiles) {
+var tasks = []
+if (!changedFiles) return tasks;
+changedFiles.forEach(function (file) {
+    if (path.extname(file) === '.js' && !~tasks.indexOf('js')) tasks.push('js')
+    if (path.extname(file) === '.scss' && !~tasks.indexOf('genCSS')) tasks.push('genCSS')
+    })
+return tasks } })      
+    .on('restart', function () {
+        console.log('restarted!')
+        })
+  .on('crash', function() {
+      console.error('Application has crashed!\n')
+      stream.emit('restart', 10)  // restart the server in 10 seconds
+      })
+cb()
+}
+
+exports.minify = minify
+exports.genCSS = genCSS
+  exports.js = js
+exports.default = series(parallel(genCSS,js), watcher)
+
---
+++ src/site.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello HTTPS!')
+})
+
+const  pkey= fs.readFileSync(__dirname+'/../server.pkey')
+const  cert= fs.readFileSync(__dirname+'/../server.cert')
+
+const credentials = {key: pkey, cert: cert};
+
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
+
+
