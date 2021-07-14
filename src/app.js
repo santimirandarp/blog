b744ea93e43571ed53509ce0f1236ad166f0524e
sm <santimirandarp@gmail.com>
2021-07-14 12:03:29 +0200

    adding details to server

--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** External packages */
+/** We use ejs render engine. Instead of html, we write EJS; instead of res.sendFile(file) we use 
+res.render(file,options) where options is an object. This allows more flexibility, and all template engines are pretty much the same */
 import ejs from 'ejs';
+/** createError is a middleware (app.use(middleware)) that we will redirect some errors to. */
 import createError from "http-errors";
 import express from "express";
 import path from "path";
@@ -30,12 +33,13 @@ useNewUrlParser: true,
                  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
                  family: 4 // Use IPv4, skip trying IPv6
 };
+
 /** Mongoose lets you start using your models immediately, without waiting for mongoose to establish a connection to MongoDB. @return promise */
-mongoose.connect(process.env.URI_DB,mongo_opts)
+const db = mongoose.connect(process.env.URI_DB,mongo_opts)
   .then(s=>console.log('connected'))
-  .catch(e=>console.error(e));
+  .catch(e=>{throw new Error(e)});
   /** catches errors after connection was established */
-  mongoose.connection.on('error', err => console.error(err));
+db.on('error', err => {throw new Error(err)});
 
   /** Import express Route Files */
   import commentsRouter from "./routes/comments.js";
@@ -61,7 +65,9 @@ mongoose.connect(process.env.URI_DB,mongo_opts)
   app.use("/font", express.static(path.join(__dirname,"public/font")));
   app.use(express.static(path.join(__dirname,"public/")));
 
-  /** enable routes for exact match on path */
+/** ROUTES. Routes will deal with errors internally, providing more context to user, 
+but if no method+route match occurs,the requests ends on the error handler below */
+
   app.use("/", indexRouter);
   app.use("/pictures", picturesRouter);
   app.use("/about", aboutRouter);
@@ -70,10 +76,11 @@ mongoose.connect(process.env.URI_DB,mongo_opts)
 
   // Only gets here if none of prev routes was a match
   // catch 404 and forward to error handler
-  app.use(function(req, res, next) { next(createError(404)); });
+  app.use((req, res, next) =>  next(createError(404)) );
 
 
-  /** Error handler. Comes directly here when we pass a parameter to next, So what does next do? Simple, it tells your app to run the next middleware. But what happens when you pass something to next? Express will abort the current stack and will run all the middleware that has 4 parameters. */
+  /** What does next do? Simple, it tells your app to run the next middleware. What happens when you pass something to next? Express will abort the current stack and will run all the middleware that has 4 parameters.(stackoverflow) 
+Error-handling middleware always takes four arguments. You must provide four arguments to identify it as an error-handling middleware function. Even if you don’t need to use the next object, you must specify it to maintain the signature. Otherwise, the next object will be interpreted as regular middleware and will fail to handle errors.(express docs)  */
 
   app.use(function(err, req, res, next) {
       // set locals, only providing error in development
--- src/routes/comments.js
+++ src/routes/comments.js
@@ -2,6 +2,7 @@ import {Comment} from "../db/models.js";
 import multiparty from "multiparty";
 import express from "express";
 const router = express.Router();
+const ISE = 'Internal Server Error. Please try again later.'
 
 /** Middleware for parsing multipart forms*/
 const parseForm = (req,res,next)=>{
@@ -11,9 +12,10 @@ const parseForm = (req,res,next)=>{
         let form  = new multiparty.Form();
         form.parse(req, (err,fields,files) => { req.body=fields; next()})
       } 
-  } catch(e){ console.error(e); next(createError(500, 'Internal Server Error. Try again later.' )) }
+  } catch(e){ next(createError(500, ISE )) }
 }
 
+/** Save comment to database using mongoose model, create method @param comment,  done. */
 const saveComment = (comment, done) => Comment.create(comment, done);
 const getComments = (skip, limit, done) => { Comment.find({})
     .sort({date:-1})
@@ -29,25 +31,27 @@ router.post('/', parseForm, (req,res,next)=>{
     let comment = { name:name[0], email:email[0], msg:message[0] };
     saveComment(comment, (err,doc) => { 
         if(err) {
-        console.error(err); 
-        next(createError(500, 'There was a problem saving the document. Please try again.'));
+        next(createError(500, "Couldn't save the document. Try again."));
         }
-        console.log(doc);
         res.json(doc) 
         })
     } catch(e) { 
-    console.error(e); 
-    next(createError(500, 'Internal Server Error. Try again later.'))
+    next(createError(500, ISE))
     }
     })
 
 router.get('/:skip/:limit', (req,res)=>{
     const {skip, limit} = req.params
     getComments(skip,limit, (err,docs)=>{
-        if(err) next(createError(500, 'Internal Server Error. Please try again later.'))
-        console.log(docs);
+        if(err) next(createError(500, ISE))
         res.json(docs);
         })
     })
 
+/** error handling for this route, error handlers always take 4 params */
+const dbError = (err,req,res,next) => {
+console.error(err)
+res.status(err.status).send(err.msg) 
+}
+router.use(dbError)
 export default router
