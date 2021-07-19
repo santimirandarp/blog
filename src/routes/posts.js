89639732aacf77e02cf8eefc2339e76f4ba85b6b
sm <santimirandarp@gmail.com>
2021-07-19 15:25:15 +0200

    fixing links on blogs

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -11,13 +11,25 @@ import date from "../views/settings/date.js";
 import {title, navigationLinks} from "../views/settings/variables.js";
 /* GET users listing. */
 
-router.use("/posts", express.static(path.join(__dirname,"posts"))
+router.use("/", express.static(path.join(__dirname,"posts"))
+
+router.get("/", (req,res)=> 
+const cssPath = "posts/posts";
+const page = "Blog Index";
+
+res.render(path, {
+      page,
+      cssPath,
+      title,
+      navigationLinks,
+      date
+}));
 
 router.get(":post", (req,res)=> 
 const post = req.params.post
 const path = `posts/${post}/${post}`
-const cssPath = ;
-const page = "about";
+const cssPath = `posts/${post}`;
+const page = `Post ${post}`;
 
 res.render(path, {
       page,
