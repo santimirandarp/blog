8ebda470b5afd177f6e014e092740d380c25a122
sm <santimirandarp@gmail.com>
2021-10-25 11:56:27 +0200

    improving the UX

--- src/public/javascripts/home/db.js
+++ src/public/javascripts/home/db.js
@@ -1,5 +1,7 @@
-import { $,mytoggler } from "../common/common.js";
+import {$,mytoggler} from "../common/common.js";
 
+//Comments Area, contains the list of comments, 
+//the post-comment form, and other buttons.
 const comments = $("#comments");
 const commentsList = comments.querySelector("#commentsList");
 const form = comments.querySelector("#form");
@@ -8,10 +10,11 @@ const loadOlderComments = comments.querySelector("#loadOlderComments");
 const thatsIt = comments.querySelector(".comments_thatsIt");
 thatsIt.style.display="none";
 
-const info = $("#comments .comments_info");
+const info = comments.querySelector(".comments_info");
 info.style.display="none";
 //
 ///** Called from postMsg, asynchronous call to DB using data from form.*/
+//Returns a promise
 const post = async(data)=>{
 const response = await fetch("/comments", {   
 method: "POST", 
@@ -24,7 +27,7 @@ return response.json();
 //
 const postMsg = e => {
   //get the elements
-  const form = $("#comments form");
+  const form = $("#comments form");//it's ok even though it is defined above also.
   const name = form.querySelector("input[name='name']");
   const email = form.querySelector("input[name='email']");
   const msg = form.querySelector("textarea[name='msg']");
@@ -34,23 +37,31 @@ const postMsg = e => {
   post(data).then(suc => { 
       form.style.display="none";
       info.style.display="block";
-      info.innerHTML=`<happy/>; ${suc.msg}. Comment will be public shortly (we show a preview).`; 
+      info.innerHTML=`<happy/>; ${suc.msg}. Your comment will be public shortly (we show a preview).`; 
       commentsList.insertAdjacentHTML("afterbegin", commentToHTML(data));
       setTimeout( ()=>{ info.style.display="none"; info.innerHTML="";},3000);
-      }).catch( () => console.log("There was an error"));
+      }).catch( () => console.log("There was an error")); //still need to deal with this error.
 };
 //
 ///** pass array of objects from database */
 const commentToDOM = docsArray => {
-  if(docsArray.length!=0){ docsArray.forEach(
-      doc => commentsList.insertAdjacentHTML("beforeend", commentToHTML(doc)));
-      return 0;
-  } else { 
+if(Array.isArray(docsArray)){
+if(docsArray.length==0){
+//displays alert to user
     thatsIt.innerHTML = "All comments were loaded.";
     thatsIt.style.display = "block";
     setTimeout(() => thatsIt.style.display = "none",3000);
     return 0;
-  } };
+} else {
+//inserts the comments
+   docsArray.forEach( doc => commentsList
+     .insertAdjacentHTML("beforeend", commentToHTML(doc)));
+      return 0;
+}} else {
+    //If it is not an array
+    thatsIt.innerHTML = "There was an error. Please try again.";
+    thatsIt.style.display = "block";
+}}
 //
 //
 ///** document => HTML elements */ 
@@ -64,6 +75,7 @@ const commentToHTML = ({name,msg,date}, preview=true) => {
 //
 const skipLimit = (nOfComments) => [nOfComments, nOfComments+10];
 const getComments = async(arr) => {
+  thatsIt.innerHTML = "Fetching Comments from Database...";
   let url = `comments/${arr[0]}/${arr[1]}`;
   const response = await fetch(url);
     return response.json();
@@ -80,7 +92,7 @@ loadOlderComments.addEventListener("click", () => {
     .catch( () =>console.error("There was a problem"));
     });
 //
-commentsList.addEventListener("click", ()=> mytoggler($("#comments form")));
+commentsList.addEventListener("click", ()=> mytoggler(form));
 form.addEventListener("submit", postMsg);
 
 
